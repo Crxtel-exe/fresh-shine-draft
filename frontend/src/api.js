adf2cbfaf89bb74908bcbe7ddfe9246a950a3aa0
Leyn <aleynsaitam@gmail.com>
2026-09-19 09:18:03 +0800

    Connect frontend to Railway backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://fresh-shine-draft-production.up.railway.app/api',
 })
 
 // Attach JWT token to every request
